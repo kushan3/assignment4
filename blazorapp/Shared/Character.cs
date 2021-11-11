55c045b100990ac60242d9318d0949d744972003
kushan3 <w0777326@myscc.ca>
2021-11-11 17:56:00 +0530

    Initial commit

---
+++ blazorapp/Shared/Character.cs
@@ -0,0 +1,8 @@
+
+public class character
+{
+    public string Title { get; set; }
+    public int Age { get; set; }
+    public string Profession { get; set; }
+    public bool IsFiction{ get; set; }
+}
